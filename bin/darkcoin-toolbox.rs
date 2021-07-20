e8847438635be5328a00a677e173d460a2e4ee0c
Yongjie Wen <93756978+Corerae@users.noreply.github.com>
2021-07-21 00:09:44 +0800

    db tables codegen

--- bin/darkcoin-toolbox.rs
+++ bin/darkcoin-toolbox.rs
@@ -25,7 +25,7 @@ async fn main() -> anyhow::Result<()> {
             let env = darkcoin::Environment::open_ro(
                 mdbx::Environment::new(),
                 &chaindata,
-                &darkcoin::tables::TABLE_MAP,
+                &darkcoin::kv::tables::TABLE_MAP,
             )?;
             let mut sizes = table_sizes(&env.begin_ro_txn()?)?
                 .into_iter()
